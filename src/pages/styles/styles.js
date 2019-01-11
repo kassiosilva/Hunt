bbabaa0263adad12670f785be7b0922fd2f70777
Kássio Silva <kassio.slva@gmail.com>
2019-01-11 15:20:05 -0300

    Adicionanado box shadow nos cards

--- src/pages/styles/styles.js
+++ src/pages/styles/styles.js
@@ -15,6 +15,14 @@ const styles = {
     borderRadius: 5,
     padding: 20,
     marginBottom: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4,
   },
 
   productTitle: {
